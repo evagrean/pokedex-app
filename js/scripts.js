d9675e70f933b3fbc696c36039d3717779b8ec3a
evagrean <54578535+evagrean@users.noreply.github.com>
2019-09-04 12:36:52 +0200

    Created object-array skeleton for repository array

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,19 @@
-var repository = [];
+var repository = [
+  {
+  name: '',
+  height: ,
+  types = ['grass','poison']
+  },
+  {
+  name: '',
+  height: ,
+  types = ['grass','poison']
+  },
+  {
+  name: '',
+  height: ,
+  types = ['grass','poison'] 
+  }
+  {}
+  {}
+];
