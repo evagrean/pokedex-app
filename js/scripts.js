71d6de89ba333b490b22e0b4522888a2de073a11
evagrean <54578535+evagrean@users.noreply.github.com>
2019-09-12 10:07:31 +0200

    Wrapped repository array in IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,45 @@
-var repository = [
-  {
-  name: 'Bulbasaur',
-  height: 2.04,
-  types: ['grass','poison'],
-  abilities: 'overgrow'
-  },
-  {
-  name: 'Weedle',
-  height: 1.00,
-  types: ['bug','poison'],
-  abilities: 'shield dust'
-  },
-  {
-  name: 'Jigglypuff',
-  height: 1.08,
-  types: ['normal', 'fairy'],
-  abilities: ['cute charm', 'competitive']
-  },
-  {
-  name: 'Nidoqueen',
-  height: 4.03,
-  types: ['poison', 'ground'],
-  abilities: ['poison point', 'rivalry']
+// IIFE Immediately Invoked Function Expression
+var pokemonRepository = (function() {
+    var repository = [
+    {
+    name: 'Bulbasaur',
+    height: 2.04,
+    types: ['grass','poison'],
+    abilities: 'overgrow'
+    },
+    {
+    name: 'Weedle',
+    height: 1.00,
+    types: ['bug','poison'],
+    abilities: 'shield dust'
+    },
+    {
+    name: 'Jigglypuff',
+    height: 1.08,
+    types: ['normal', 'fairy'],
+    abilities: ['cute charm', 'competitive']
+    },
+    {
+    name: 'Nidoqueen',
+    height: 4.03,
+    types: ['poison', 'ground'],
+    abilities: ['poison point', 'rivalry']
+    }
+  ];
+
+  function getAll() {
+    return repository;
   }
-];
+
+  function add(pokemon) {
+    repository.push(pokemon);
+  }
+
+  return {
+    getAll: getAll,
+    add: add
+  };
+})()
 
 // forEach Loop function to print details of each Pokemon
 
