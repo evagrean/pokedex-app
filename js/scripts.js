d944868707ebf71cb94534634abd86621f7bcc1f
evagrean <54578535+evagrean@users.noreply.github.com>
2019-09-17 10:32:42 +0200

    Removed array of pokemon objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,6 @@
 // IIFE Immediately Invoked Function Expression
 var pokemonRepository = (function() {
-    var repository = [
-    {
-    name: 'Bulbasaur',
-    height: 2.04,
-    types: ['grass','poison'],
-    abilities: 'overgrow'
-    },
-    {
-    name: 'Weedle',
-    height: 1.00,
-    types: ['bug','poison'],
-    abilities: 'shield dust'
-    },
-    {
-    name: 'Jigglypuff',
-    height: 1.08,
-    types: ['normal', 'fairy'],
-    abilities: ['cute charm', 'competitive']
-    },
-    {
-    name: 'Nidoqueen',
-    height: 4.03,
-    types: ['poison', 'ground'],
-    abilities: ['poison point', 'rivalry']
-    }
-  ];
+    var repository = [];
 
   function getAll() {
     return repository;
